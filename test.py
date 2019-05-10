bfe2da0be62aba016d44e382d0032aff63bd9d4f
김준석 <johnkim@tmon.co.kr>
2019-05-10 11:41:46 +0900

    test 코드 속도 개선

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+from multiprocessing import Queue, Process
 
 import numpy as np
 import scipy.stats
@@ -10,36 +12,39 @@ from utils import read_data
 from variables import VALUE_UNK
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input_dir', required=True)
-    parser.add_argument('--model_dir', required=True)
-    parser.add_argument('--gpu_num', default=1, type=int)
-    parser.add_argument('--specific_weight', default='')
-    parser.add_argument('--batch_size', default=10, type=int)
-    parser.add_argument('--test_steps', default=10, type=int)
+class Worker(Process):
+    def __init__(self, gpuid, queue, result_queue, model_dir, specific_weight):
+        Process.__init__(self, name='ModelProcessor')
+        self._gpuid = gpuid
+        self._queue = queue
+        self._result_queue = result_queue
+        self._model_dir = model_dir
+        self._specific_weight = specific_weight
 
-    conf = parser.parse_args()
-    model, core_model, _ = load_model(conf.model_dir, conf.specific_weight, conf.gpu_num, test_only=True)
-    core_model.summary(line_length=200)
+    def run(self):
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(self._gpuid)
+        model, _, _ = load_model(self._model_dir, self._specific_weight, test_only=True)
+        while True:
+            data = self._queue.get()
+            if data is None:
+                self._queue.put(None)
+                break
+            result = self.predict(model, *data)
+            self._result_queue.put(result)
 
-    data, _, params = read_data(conf.input_dir)
-    max_len = model.input_shape[0][1]
-    generator = DataGenerator(data, params, conf.batch_size, max_len, mask_rate=0.0, data_type='test')
+        self._result_queue.put(None)
 
-    NDCG = 0.0
-    HT1 = 0.0
-    HT5 = 0.0
-    HT10 = 0.0
-    valid_user = 0
-    unknown = 0.0
-    for idx in tqdm(range(conf.test_steps)):
-        inputs, outputs = generator[idx]
-        predicts = model.predict(inputs)
+    def predict(self, model, inputs, outputs):
+        predicts = model.predict(inputs, batch_size=inputs[-1].shape[0])
         outputs = outputs[0][:, -1, 0]
         ranks = scipy.stats.mstats.rankdata(predicts[:, -1, :] * -1, axis=1)
-        batch_size, seq_len = inputs[-1].shape
-        for i in range(batch_size):
+        NDCG = 0.0
+        HT1 = 0.0
+        HT5 = 0.0
+        HT10 = 0.0
+        unknown = 0.0
+        for i in range(len(outputs)):
             if outputs[i] == VALUE_UNK:
                 unknown += 1
                 continue
@@ -52,14 +57,93 @@ def main():
                 HT10 += 1
                 NDCG += 1 / np.log2(rank + 2)
 
-        valid_user += batch_size
+        return NDCG, HT1, HT5, HT10, unknown
 
-    NDCG /= valid_user
-    HT1 /= valid_user
-    HT5 /= valid_user
-    HT10 /= valid_user
-    unknown /= valid_user
-    print('NDCG = {}\nHT1 = {}\nHT5 = {}\nHT10 = {}\nUNK = {}'.format(NDCG, HT1, HT5, HT10, unknown))
+
+class Collector(Process):
+    def __init__(self, gpu_num, result_queue, valid_user, test_steps):
+        Process.__init__(self, name='Collector')
+        self._result_queue = result_queue
+        self._valid_user = valid_user
+        self._test_steps = test_steps
+        self._gpu_num = gpu_num
+
+    def run(self):
+        NDCG = 0.0
+        HT1 = 0.0
+        HT5 = 0.0
+        HT10 = 0.0
+        unknown = 0.0
+
+        none_count = 0
+        pbar = tqdm(total=self._test_steps)
+        while True:
+            result = self._result_queue.get()
+            if result is None:
+                none_count += 1
+                if none_count == self._gpu_num:
+                    break
+            else:
+                NDCG += result[0]
+                HT1 += result[1]
+                HT5 += result[2]
+                HT10 += result[3]
+                unknown += result[4]
+                pbar.update(1)
+        pbar.close()
+
+        NDCG /= self._valid_user
+        HT1 /= self._valid_user
+        HT5 /= self._valid_user
+        HT10 /= self._valid_user
+        unknown /= self._valid_user
+        print('NDCG = {}\nHT1 = {}\nHT5 = {}\nHT10 = {}\nUNK = {}'.format(NDCG, HT1, HT5, HT10, unknown))
+
+
+class Scheduler:
+    def __init__(self, gpuids, model_dir, specific_weight, valid_user, test_steps):
+        self._queue = Queue()
+        self._result_queue = Queue()
+        self._workers = list()
+        self._valid_user = valid_user
+        for gpuid in gpuids:
+            self._workers.append(Worker(gpuid, self._queue, self._result_queue, model_dir, specific_weight))
+        self._collector = Collector(len(gpuids), self._result_queue, valid_user, test_steps)
+
+    def start(self, data_list):
+        for data in data_list:
+            self._queue.put(data)
+        self._queue.put(None)
+
+        for worker in self._workers:
+            worker.start()
+        self._collector.start()
+
+        for worker in self._workers:
+            worker.join()
+        self._collector.join()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_dir', required=True)
+    parser.add_argument('--model_dir', required=True)
+    parser.add_argument('--gpu_num', default=1, type=int)
+    parser.add_argument('--specific_weight', default='')
+    parser.add_argument('--batch_size', default=10, type=int)
+    parser.add_argument('--test_steps', default=10, type=int)
+
+    conf = parser.parse_args()
+
+    data, _, params = read_data(conf.input_dir)
+    # max_len = model.input_shape[0][1]
+    max_len = 30
+    generator = DataGenerator(data, params, conf.batch_size, max_len, mask_rate=0.0, data_type='test')
+
+    valid_user = conf.batch_size * conf.test_steps
+    scheduler = Scheduler(list(range(conf.gpu_num)), conf.model_dir, conf.specific_weight, valid_user, conf.test_steps)
+    data_list = [generator[idx] for idx in range(conf.test_steps)]
+    scheduler.start(data_list)
 
 
 if __name__ == '__main__':
