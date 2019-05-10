3369b7b89463543692a3b1e8150d487f4f1bb7b9
김준석 <johnkim@tmon.co.kr>
2019-05-10 14:56:05 +0900

    test 코드 속도 개선 +2

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import argparse
+import concurrent.futures
+import json
 import os
-from multiprocessing import Queue, Process
+from math import ceil
 
 import numpy as np
 import scipy.stats
@@ -12,106 +14,49 @@ from utils import read_data
 from variables import VALUE_UNK
 
 
-class Worker(Process):
-    def __init__(self, gpuid, queue, result_queue, model_dir, specific_weight):
-        Process.__init__(self, name='ModelProcessor')
-        self._gpuid = gpuid
-        self._queue = queue
-        self._result_queue = result_queue
-        self._model_dir = model_dir
-        self._specific_weight = specific_weight
-
-    def run(self):
+def predict(data_list, model_dir, specific_weight, show_summary=True, gpuid=None):
+    if gpuid is not None:
         os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-        os.environ["CUDA_VISIBLE_DEVICES"] = str(self._gpuid)
-        model, _, _ = load_model(self._model_dir, self._specific_weight, test_only=True)
-        while True:
-            data = self._queue.get()
-            if data is None:
-                self._queue.put(None)
-                break
-            result = self.predict(model, *data)
-            self._result_queue.put(result)
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(gpuid)
 
-        self._result_queue.put(None)
+    model, _, _ = load_model(model_dir, specific_weight, test_only=True)
+    if show_summary:
+        model.summary(line_length=200)
 
-    def predict(self, model, inputs, outputs):
-        predicts = model.predict(inputs, batch_size=inputs[-1].shape[0])
+    NDCG = 0.0
+    HT1 = 0.0
+    HT5 = 0.0
+    HT10 = 0.0
+    valid_user = 0
+    unknown = 0.0
+    if show_summary:
+        data_list = tqdm(data_list)
+    for data in data_list:
+        inputs, outputs = data
+        predicts = model.predict(inputs)
         outputs = outputs[0][:, -1, 0]
         ranks = scipy.stats.mstats.rankdata(predicts[:, -1, :] * -1, axis=1)
-        return outputs, ranks
+        batch_size, seq_len = inputs[-1].shape
+        for i in range(batch_size):
+            if outputs[i] == VALUE_UNK:
+                unknown += 1
+                continue
+            rank = ranks[i][outputs[i]]
+            if rank <= 1:
+                HT1 += 1
+            if rank <= 5:
+                HT5 += 1
+            if rank <= 10:
+                HT10 += 1
+                NDCG += 1 / np.log2(rank + 2)
 
-
-class Collector(Process):
-    def __init__(self, gpu_num, result_queue, valid_user, test_steps):
-        Process.__init__(self, name='Collector')
-        self._result_queue = result_queue
-        self._valid_user = valid_user
-        self._test_steps = test_steps
-        self._gpu_num = gpu_num
-
-    def run(self):
-        NDCG = 0.0
-        HT1 = 0.0
-        HT5 = 0.0
-        HT10 = 0.0
-        unknown = 0.0
-
-        none_count = 0
-        pbar = tqdm(total=self._test_steps)
-        while True:
-            result = self._result_queue.get()
-            if result is None:
-                none_count += 1
-                if none_count == self._gpu_num:
-                    break
-            else:
-                outputs, ranks = result
-                for i in range(len(outputs)):
-                    if outputs[i] == VALUE_UNK:
-                        unknown += 1
-                        continue
-                    rank = ranks[i][outputs[i]]
-                    if rank <= 1:
-                        HT1 += 1
-                    if rank <= 5:
-                        HT5 += 1
-                    if rank <= 10:
-                        HT10 += 1
-                        NDCG += 1 / np.log2(rank + 2)
-                pbar.update(1)
-        pbar.close()
-
-        NDCG /= self._valid_user
-        HT1 /= self._valid_user
-        HT5 /= self._valid_user
-        HT10 /= self._valid_user
-        unknown /= self._valid_user
-        print('NDCG = {}\nHT1 = {}\nHT5 = {}\nHT10 = {}\nUNK = {}'.format(NDCG, HT1, HT5, HT10, unknown))
-
-
-class Scheduler:
-    def __init__(self, gpuids, model_dir, specific_weight, valid_user, test_steps):
-        self._queue = Queue()
-        self._result_queue = Queue()
-        self._workers = list()
-        self._valid_user = valid_user
-        for gpuid in gpuids:
-            self._workers.append(Worker(gpuid, self._queue, self._result_queue, model_dir, specific_weight))
-        self._collector = Collector(len(gpuids), self._result_queue, valid_user, test_steps)
-
-    def start(self, data_list):
-        for data in data_list:
-            self._queue.put(data)
-        self._queue.put(None)
-
-        for worker in self._workers:
-            worker.start()
-        self._collector.start()
-
-        for worker in self._workers:
-            worker.join()
-        self._collector.join()
+        valid_user += batch_size
+    NDCG /= valid_user
+    HT1 /= valid_user
+    HT5 /= valid_user
+    HT10 /= valid_user
+    unknown /= valid_user
+    return NDCG, HT1, HT5, HT10, unknown
 
 
 def main():
@@ -126,14 +71,30 @@ def main():
     conf = parser.parse_args()
 
     data, _, params = read_data(conf.input_dir)
-    # max_len = model.input_shape[0][1]
-    max_len = 30
-    generator = DataGenerator(data, params, conf.batch_size, max_len, mask_rate=0.0, data_type='test')
+    with open(os.path.join(conf.model_dir, "config.json"), "r") as f:
+        old_conf = json.load(f)
+        max_len = old_conf['max_len']
+    generator = DataGenerator(data, params, int(conf.batch_size / conf.gpu_num), max_len, mask_rate=0.0,
+                              data_type='test')
+    data_list = [generator[idx] for idx in range(conf.test_steps * conf.gpu_num)]
+    if conf.gpu_num == 1:
+        NDCG, HT1, HT5, HT10, unknown = predict(data_list, conf.model_dir, conf.specific_weight, show_summary=True,
+                                                gpuid=None)
+    else:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=conf.gpu_num) as executor:
+            data_size = len(data_list)
+            per_data = int(ceil(data_size / conf.gpu_num))
+            futures = []
+            for idx in range(conf.gpu_num):
+                sub_data_list = data_list[per_data * idx: min(per_data * (idx + 1), data_size)]
+                future = executor.submit(predict, sub_data_list, conf.model_dir, conf.specific_weight,
+                                         show_summary=idx == 0, gpuid=idx)
+                futures.append(future)
 
-    valid_user = conf.batch_size * conf.test_steps
-    scheduler = Scheduler(list(range(conf.gpu_num)), conf.model_dir, conf.specific_weight, valid_user, conf.test_steps)
-    data_list = [generator[idx] for idx in range(conf.test_steps)]
-    scheduler.start(data_list)
+            result = np.array([future.result() for future in futures]).sum(axis=0) / conf.gpu_num
+            NDCG, HT1, HT5, HT10, unknown = result
+
+    print('NDCG = {}\nHT1 = {}\nHT5 = {}\nHT10 = {}\nUNK = {}'.format(NDCG, HT1, HT5, HT10, unknown))
 
 
 if __name__ == '__main__':
