42bc08e1615da4d5e58d55ca1872bb53c66abb32
김준석 <johnkim@tmon.co.kr>
2019-05-13 16:45:55 +0900

    모델 저장 오류 수정

--- custom_model_checkpoint.py
+++ custom_model_checkpoint.py
@@ -8,6 +8,14 @@ class CustomModelCheckpoint(Callback):
         self._last_saver = keras.callbacks.ModelCheckpoint(last_path)
         pass
 
+    def set_params(self, params):
+        self._weight_saver.set_params(params)
+        self._last_saver.set_params(params)
+
+    def set_model(self, model):
+        self._weight_saver.set_model(model)
+        self._last_saver.set_model(model)
+
     def on_epoch_end(self, epoch, logs=None):
         self._weight_saver.on_epoch_end(epoch, logs)
         self._last_saver.on_epoch_end(epoch, logs)
